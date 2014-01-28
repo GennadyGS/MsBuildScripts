38c33820c55858f90a06b34a146b7c7d07b8cf26
Gennadii Saltyshchak <GennadyGS@gmail.com>
2014-01-28 15:39:16 +0200

    msb.ps1: code outline

--- msb.ps1
+++ msb.ps1
@@ -1,12 +1,15 @@
-function charIsSwitch([char] $c) {
+function CharIsSwitch([char] $c) 
+{
     $switchChars = '-/';
     return $switchChars.Contains($c);
 }
 
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $proj = Get-Item .\* -include ('*.sln', '*.csproj')
-if ($proj) {
-    $msbArgs = $args | foreach {if (charIsSwitch($_.ToString()[0])) {$_} else {"/t:$_"}};
+if ($proj) 
+{
+    $msbArgs = $args |
+        foreach {if (charIsSwitch $_.ToString()[0]) {$_} else {"/t:$_"}};
     $scriptArgs = @($proj[0].Name) + $msbArgs;
     $scriptFile = $scriptPath + '\XMSBuild.cmd'
     Start-Process $scriptFile -ArgumentList $scriptArgs
